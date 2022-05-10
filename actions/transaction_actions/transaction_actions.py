74b7a560528ae861178ee859e32abdff45685341
haruncansurav <can.surav@ug.bilkent.edu.tr>
2022-05-10 17:14:06 +0300

    Transaction services completed

--- actions/transaction_actions/transaction_actions.py
+++ actions/transaction_actions/transaction_actions.py
@@ -10,6 +10,7 @@ def connect():
 	#Define our connection string
 	conn_string = "host='localhost' dbname='testdb3' user='postgres' password='admin'"
 	conn = psycopg2.connect(conn_string)
+	conn.autocommit = True
 	return conn.cursor()
 
 # Connect to the database
@@ -20,25 +21,75 @@ if connection_instance == 0:
         abort(500)
     print("Connected!")
 
-class Transaction(Resource):
+class DepositingTransaction(Resource):
 
-    def get(self, user_id):
-        connection_instance.execute("SELECT * FROM transaction INNER JOIN made_by ON transaction.transaction_id = made_by.transaction_id WHERE user_id = %s", (user_id,))
+    def get(self, account_id):
+        connection_instance.execute("SELECT * FROM transaction INNER JOIN made_by ON transaction.transaction_id = made_by.transaction_id WHERE user_id = {} AND transaction_type LIKE 'DP%'".format(account_id.__str__(),))
         transaction = connection_instance.fetchall()
         if transaction:
-            return transaction
+            if len(transaction) == 0:
+                return "No transactions"
+            
+            # Parsing the account data to a JSON format with columns as keys
+            transaction_data = []
+            for i in range(len(transaction)):
+                transaction_data.append({})
+                for j in range(len(transaction[i])):
+                    transaction_data[i][connection_instance.description[j][0]] = transaction[i][j].__str__()
+            return transaction_data
         else:
-            abort(404, message="Transaction {} doesn't exist".format(transaction_id))
+            abort(404, message="User {} does not have any depositing transactions".format(account_id))
 
     def post(self, account_id):
         parser = reqparse.RequestParser()
-        parser.add_argument('transaction_id', type=int)
         parser.add_argument('amount', type=float)
         parser.add_argument('transaction_type', type=str)
         parser.add_argument('date', type=str)
         args = parser.parse_args()
-        connection_instance.execute("INSERT INTO transaction (transaction_id, amount, transaction_type, date) VALUES (%s, %s, %s, %s)", (args['transaction_id'], args['amount'], args['transaction_type'], args['date']))
-        connection_instance.execute("INSERT INTO made_by (transaction_id, user_id) VALUES (%s, %s)", (args['transaction_id'], account_id))
+        # Getting the current transaction id from the database
+        connection_instance.execute("SELECT MAX(transaction_id) FROM transaction")
+        transaction_id = connection_instance.fetchone()[0]
+        if transaction_id is None:
+            transaction_id = 1
+        else:
+            transaction_id += 1
+        connection_instance.execute("INSERT INTO transaction (transaction_id, amount, transaction_type, date) VALUES (%s, %s, %s, %s)", (transaction_id, args['amount'], args['transaction_type'], args['date']))
+        connection_instance.execute("INSERT INTO made_by (transaction_id, user_id) VALUES (%s, %s)", (transaction_id, account_id))
         return args
 
+
+class WithdrawingTransaction(Resource):
     
+    def get(self, account_id):
+        connection_instance.execute("SELECT * FROM transaction INNER JOIN made_by ON transaction.transaction_id = made_by.transaction_id WHERE user_id = {} AND transaction_type LIKE 'WD%'".format(account_id.__str__(),))
+        transaction = connection_instance.fetchall()
+        if transaction:
+            if len(transaction) == 0:
+                return "No transactions"
+            
+            # Parsing the account data to a JSON format with columns as keys
+            transaction_data = []
+            for i in range(len(transaction)):
+                transaction_data.append({})
+                for j in range(len(transaction[i])):
+                    transaction_data[i][connection_instance.description[j][0]] = transaction[i][j].__str__()
+            return transaction_data
+        else:
+            abort(404, message="User {} does not have any depositing transactions".format(account_id))
+
+    def post(self, account_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('amount', type=float)
+        parser.add_argument('transaction_type', type=str)
+        parser.add_argument('date', type=str)
+        args = parser.parse_args()
+        # Getting the current transaction id from the database
+        connection_instance.execute("SELECT MAX(transaction_id) FROM transaction")
+        transaction_id = connection_instance.fetchone()[0]
+        if transaction_id is None:
+            transaction_id = 1
+        else:
+            transaction_id += 1
+        connection_instance.execute("INSERT INTO transaction (transaction_id, amount, transaction_type, date) VALUES (%s, %s, %s, %s)", (transaction_id, args['amount'], args['transaction_type'], args['date']))
+        connection_instance.execute("INSERT INTO made_by (transaction_id, user_id) VALUES (%s, %s)", (transaction_id, account_id))
+        return args
--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from actions.transaction_actions.transaction_actions import Transaction
+from actions.transaction_actions.transaction_actions import DepositingTransaction, WithdrawingTransaction
 from actions.account_actions.account_actions import Account, CreditCard
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Transaction, '/transaction/<int:user_id>')
+api.add_resource(DepositingTransaction, '/depositing_transaction/<int:account_id>')
+api.add_resource(WithdrawingTransaction, '/withdrawing_transaction/<int:account_id>')
 api.add_resource(CreditCard, '/credit_card/<int:account_id>')
 api.add_resource(Account, '/accounts/<int:account_id>')
 
